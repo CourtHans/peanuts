0f6ea20982ac007c93a1d53dd5db1ace726e4874
CourtHans <cornta@yahoo.com>
2022-02-09 03:30:09 +0000

    silly commit message

--- pythonFun.py
+++ pythonFun.py
@@ -6,3 +6,15 @@
 # Purpose: 
 
 print ("Demo of python.")
+my_list = ["1 potato", "2 potato", "3 potato", "4"]
+
+print ("here's the full list!!")
+print (my_list)
+
+print ("this is just the 2nd element")
+print (my_list[1])
+
+print ("making a change")
+my_list[3] = "hooray!!"
+print ("")
+print (my_list)
