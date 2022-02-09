bb59f150b9e5271ff0e72f7cfa6d952bd5165dca
CourtHans <cornta@yahoo.com>
2022-02-09 05:03:14 +0000

    bash scripting and variables!

--- bashFun.sh
+++ bashFun.sh
@@ -1,2 +1,14 @@
-#!/usr/bin/env bash
-echo "this is just for funt"
+#!/bin/bash
+
+# Script:                       Ops fun
+# Author:                       Courtney Hans
+# Date of latest revision:      02/08/22
+# Purpose:                      commands
+
+echo "this is just for fun"
+
+current_date=$(date)
+echo $current_date
+
+echo "today is $current_date" > date.txt
+
