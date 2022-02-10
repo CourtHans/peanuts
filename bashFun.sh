8c6515d786aac04f39b4028b78f054940580f6f6
CourtHans <cornta@yahoo.com>
2022-02-10 06:25:29 +0000

    still playing around

--- bashFun.sh
+++ bashFun.sh
@@ -6,9 +6,12 @@
 # Purpose:                      commands
 
 echo "this is just for fun"
-
+echo 
 current_date=$(date)
 echo $current_date
+echo
+ping_here=$(ping -c 4 8.8.8.8)
+echo $ping_here
 
-echo "today is $current_date" > date.txt
+#echo "today is $current_date" > date.txt
 
